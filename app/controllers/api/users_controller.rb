2076f66799160c1e4e5adfb7efa55ba5afdabd62
prestontewell <preston@tewellphoto.com>
2019-03-28 11:39:50 -0500

    add user create action, route and validations. comment protect from forgery. add username to User model and migrate

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,18 @@
 class Api::UsersController < ApplicationController
   
+  def create
+    user = User.new(
+      name: "Preston Tewell",
+      email: "prestontewell@gmail.com",
+      username: "the_dude",
+      password: "password",
+      password_confirmation: "password",
+      avatar_url: "null",
+    )
+    if user.save
+      render json: {message: 'User created successfully'}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+
+    post '/users' => 'users#create'
+
+  end
 end
