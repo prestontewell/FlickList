782e5b9e9c6646e8c79b860ad3e2a54000337c6d
prestontewell <preston@tewellphoto.com>
2019-04-08 16:05:11 -0500

    update titles index with API storage

--- app/controllers/api/titles_controller.rb
+++ app/controllers/api/titles_controller.rb
@@ -2,7 +2,7 @@ class Api::TitlesController < ApplicationController
 
   def index
     search = params[:search]
-    @titles = HTTP.get("https://api.themoviedb.org/3/search/multi?api_key=&language=en-US&query=#{params[:search]}&page=1&include_adult=false")
+    @titles = HTTP.get("https://api.themoviedb.org/3/search/multi?api_key=#{ENV["API_KEY"]}&language=en-US&query=#{params[:search]}&page=1&include_adult=false")
     render json: @titles.parse
   end  
 
