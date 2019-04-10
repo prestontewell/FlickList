0f7eb8e56a173339f4211d6acdf7e8a7deee04d0
prestontewell <preston@tewellphoto.com>
2019-04-10 14:50:23 -0500

    fix http gem by switching to httparty

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.6.1'
 
 gem 'http'
+gem 'httparty'
 
 gem 'figaro'
 
--- app/controllers/api/titles_controller.rb
+++ app/controllers/api/titles_controller.rb
@@ -2,8 +2,8 @@ class Api::TitlesController < ApplicationController
 
   def index
     search = params[:search]
-    @titles = HTTP.get("https://api.themoviedb.org/3/search/multi?api_key=#{ENV["API_KEY"]}&language=en-US&query=#{params[:search]}&page=1&include_adult=false")
-    render json: @titles.parse
+    @titles = HTTParty.get ("https://api.themoviedb.org/3/search/multi?api_key=#{ENV["API_KEY"]}&language=en-US&query=#{params[:search]}&page=1&include_adult=false"), format: :plain
+    render json: @titles.body
   end  
 
 end
